4560589eadef3f09590887158fce2bc8c4f3333a
praveenvgr07 <praveenvgr07@gmail.com>
2026-02-15 12:04:59 +0530

    first

---
+++ index.js
@@ -0,0 +1,79 @@
+const r=require("express");
+const a=r();
+const hbs=require("hbs");
+const mysql=require("mysql2");
+const Path=require("path");
+const env = require("dotenv");
+ env.config({
+    path: Path.join(__dirname,".env")
+  });    
+
+const location=Path.join(__dirname,"./public");
+
+a.listen(1234,(req,res)=>{
+    console.log("port connected");
+});
+
+
+a.set("view engine","hbs");
+
+
+a.use(r.static(location));
+a.set("views",Path.join(__dirname,"./public/views"));
+
+a.get("/",(req,res)=>{
+    res.render("home" ,{msg:process.env.goldrate});
+
+});
+
+a.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+
+a.get("/login/register",(req,res)=>{
+    res.render("register");
+});
+a.get("/home",(req,res)=>{
+    res.render("home");
+});
+ 
+ 
+const db =mysql.createConnection({
+    port:process.env.port,
+    host:process.env.host,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database,
+});
+
+
+db.connect((err,res)=>{
+    if(err) throw err;
+    else console.log("database connected");
+});
+
+
+a.use(r.urlencoded({extended:false}));
+
+ a.post("/form",(req,res)=>{
+        console.log(req.body); 
+        db.query("select *from node_js where username=? and password=?",[req.body.username,req.body.password],(err,res)=>{
+            if(res.length==0){console.log("data not found")};
+          console.log(res);
+          res.forEach(e => {
+                console.log(e.username,"\t",e.password)
+          });
+        })
+
+ }) ;
+
+ a.post("/register",(req,res)=>{    
+    console.log(req.body);
+            db.query("insert into node_js values(?,?,?,?)",[req.body.username,req.body.phone_number,req.body.email,req.body.password],(err,res)=>{
+            if(err) throw err;
+            console.log("data inserted");
+            });
+
+            });
+       
